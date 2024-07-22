3fe905342a039e00435d4eb1c5dfcbad41dac623
Alaric Henrot <alaric.henrot@ospi.fr>
2024-07-22 15:29:45 +0200

    add cowsay dependencie

---
+++ index.php
@@ -0,0 +1,10 @@
+<?php
+require 'vendor/autoload.php';
+use CowSay\Cow;
+
+$bessie = new Cow('sheeeeeesh');
+$bessie->setTongue('U');
+
+
+// or just echo the object for direct output
+echo $bessie;
